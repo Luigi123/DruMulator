875807b8fb4437f1feae3b03a7a05fb66ae7aa74
Luigi Taira D <luigiwd@gmail.com>
2017-11-16 16:29:28 +0900

    transform ES2015+ with babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,9 @@ module.exports = {
       path: path.join(__dirname, "dist"),
       filename: "bundle.js"
   },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  }
 }
