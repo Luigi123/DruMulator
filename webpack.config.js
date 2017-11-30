02d271c1ad0584dcb846585132907150af79280f
Luigi Taira D <luigiwd@gmail.com>
2017-11-30 16:51:48 +0900

    move static files to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
   entry: "./src/script.js",
   output: {
-      path: path.join(__dirname, "dist"),
+      path: path.join(__dirname, "dist", "js"),
       filename: "bundle.js"
   },
   module: {
